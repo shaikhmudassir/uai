8acb24066e9394e88474ebe2b670a7e4d5c00931
shaikh-debian <skmudassirahmed@gmail.com>
2022-10-10 09:05:36 +0530

    Login & Register Designs Added

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from attendance.views import ScanView, HomeView, CreateView
+from attendance.views import *
 
 app_name = "attendance"
 urlpatterns = [
 	path("", HomeView.as_view(), name="home"),
 	path("create", CreateView.as_view(), name="create"),
 	path("scan", ScanView.as_view(), name="scan"),
+	path("login", Login.as_view(), name="login"),
+	path("register", Register.as_view(), name="register"),
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import get_user_model, authenticate
 from django.views.generic import TemplateView
 from django.views import View
+from django.shortcuts import render
 import pyqrcode, io
 
 
@@ -24,3 +27,44 @@ class ScanView(TemplateView):
 
 	template_name = "attendance/index.html"
 
+
+class Login(View):
+    def get(self, request):
+        # if 'user_id' in request.session:
+        #     return HttpResponseRedirect('')
+        
+        return render(request, 'attendance/login.html', {
+        })
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            request.session['user_id'] = user.id
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('login')
+
+class Register(View):
+    def post(self, request, *args, **kwargs):
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        roll_no = request.POST['roll_no']
+        year = request.POST['year']
+        branch = request.POST['branch']
+        batch = request.POST['batch']
+        
+        print(request.post)
+        
+        return HttpResponseRedirect('login')
+
+class Logout(View):
+    def get(self, request):
+        try:
+            del request.session['user_id']
+            return HttpResponseRedirect('login')
+        except KeyError as e:
+            print(e)
+            return HttpResponseRedirect('/')
